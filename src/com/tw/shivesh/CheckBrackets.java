f360afa455d5a0390063ad3fa583eac3a0502f22
Shivesh Gupta <GuptaShivesh@johndeere.com>
2015-09-04 15:45:56 +0530

    parser for parenthesis check

---
+++ src/com/tw/shivesh/CheckBrackets.java
@@ -0,0 +1,67 @@
+package com.tw.shivesh;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class CheckBrackets {
+
+	public static void main(String[] args) {
+		System.out.println(checkBrackets("a*(([x + 2])[(y)])+2"));
+		System.out.println(checkBrackets("b/((][x)"));
+		System.out.println(checkBrackets("(5 + 2))("));
+		System.out.println(checkBrackets("([x +2]))y(z)-5"));
+		System.out.println(checkBrackets("([x+2)"));
+	}
+
+	private static boolean checkBrackets(String string) {
+
+		Map<Character, Character> bracketPair = new HashMap<Character, Character>();
+		bracketPair.put(')', '(');
+		bracketPair.put(']', '[');
+		
+		List<Character> permittedBrackets = new ArrayList<Character>();
+		permittedBrackets.add('(');
+		permittedBrackets.add(')');
+		permittedBrackets.add('[');
+		permittedBrackets.add(']');
+		List<Character> onlyBrackets = getOnlyBrackets(string,permittedBrackets);
+		
+		if (onlyBrackets.size() % 2 == 0 && checkBracketLogic(onlyBrackets, bracketPair)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static List<Character> getOnlyBrackets(String string,List<Character> permittedBrackets) {
+		List<Character> onlyBrackets = new ArrayList<Character>();
+		char[] userInput = string.toCharArray();
+		for (char c : userInput) {
+			if (permittedBrackets.contains(c)) {
+				onlyBrackets.add(c);
+			}
+		}
+		return onlyBrackets;
+	}
+
+	private static boolean checkBracketLogic(List<Character> brackets,Map<Character, Character> map) {
+		Stack<Character> bracketStack = new Stack<Character>();
+		for (Character eachBracket : brackets) {
+			if (eachBracket == '(' || eachBracket == '[') {
+				bracketStack.push(eachBracket);
+			} else if(!bracketStack.empty()) {
+				map.get(eachBracket).equals(bracketStack.peek());
+				bracketStack.pop();
+			}else{
+				return false;
+			}
+		}
+		if (bracketStack.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+}
